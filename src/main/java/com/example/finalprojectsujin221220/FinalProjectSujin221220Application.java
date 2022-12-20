b7bd138b076c96d775df1131cc9d2331ad09c568
이수진 <slrkajsep337@naver.com>
2022-12-20 12:41:02 +0900

    initial commit

---
+++ src/main/java/com/example/finalprojectsujin221220/FinalProjectSujin221220Application.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectsujin221220;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectSujin221220Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectSujin221220Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalprojectsujin221220/FinalProjectSujin221220ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectsujin221220;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectSujin221220ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
