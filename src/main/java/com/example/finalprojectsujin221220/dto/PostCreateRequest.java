031334246c61de71500a961a92f25926eab59035
이수진 <slrkajsep337@naver.com>
2022-12-27 16:49:27 +0900

    chore : 어노테이션 추가

--- src/main/java/com/example/finalprojectsujin221220/dto/PostCreateRequest.java
+++ src/main/java/com/example/finalprojectsujin221220/dto/PostCreateRequest.java
@@ -1,11 +1,14 @@
 package com.example.finalprojectsujin221220.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Getter
 public class PostCreateRequest {
 
--- src/main/java/com/example/finalprojectsujin221220/dto/PostModifyRequest.java
+++ src/main/java/com/example/finalprojectsujin221220/dto/PostModifyRequest.java
@@ -1,10 +1,14 @@
 package com.example.finalprojectsujin221220.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Builder
 public class PostModifyRequest {
 
     private String title;
