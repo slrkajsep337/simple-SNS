0560fb147e425e6f3206379ef8c7cad9bc3da4c3
이수진 <slrkajsep337@naver.com>
2023-01-04 00:33:22 +0900

    feat: comment 수정 기능

--- src/main/java/com/example/finalprojectsujin221220/controller/CommentController.java
+++ src/main/java/com/example/finalprojectsujin221220/controller/CommentController.java
@@ -4,6 +4,8 @@ package com.example.finalprojectsujin221220.controller;
 import com.example.finalprojectsujin221220.domain.Response;
 import com.example.finalprojectsujin221220.dto.CommentCreateRequest;
 import com.example.finalprojectsujin221220.dto.CommentCreateResponse;
+import com.example.finalprojectsujin221220.dto.CommentModifyRequest;
+import com.example.finalprojectsujin221220.dto.CommentModifyResponse;
 import com.example.finalprojectsujin221220.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -19,6 +21,11 @@ public class CommentController {
     @PostMapping("/posts/{postId}/comments")
     public Response<CommentCreateResponse> newComment(@PathVariable Long postId, @RequestBody CommentCreateRequest dto, Authentication authentication) {
         return Response.success(cs.newComment(postId, dto, authentication));
+    }
+
+    @PutMapping("/posts/{postId}/comments/{id}")
+    public Response<CommentModifyResponse> modifyComment(@PathVariable Long postId, @PathVariable Long id, @RequestBody CommentModifyRequest dto, Authentication authentication) {
+        return Response.success(cs.modifyComment(postId, id, dto, authentication));
     }
 
 
--- src/main/java/com/example/finalprojectsujin221220/service/CommentService.java
+++ src/main/java/com/example/finalprojectsujin221220/service/CommentService.java
@@ -5,6 +5,10 @@ import com.example.finalprojectsujin221220.domain.entity.Post;
 import com.example.finalprojectsujin221220.domain.entity.User;
 import com.example.finalprojectsujin221220.dto.CommentCreateRequest;
 import com.example.finalprojectsujin221220.dto.CommentCreateResponse;
+import com.example.finalprojectsujin221220.dto.CommentModifyRequest;
+import com.example.finalprojectsujin221220.dto.CommentModifyResponse;
+import com.example.finalprojectsujin221220.exception.ApplicationException;
+import com.example.finalprojectsujin221220.exception.ErrorCode;
 import com.example.finalprojectsujin221220.repository.CommentRepository;
 import com.example.finalprojectsujin221220.repository.PostRepository;
 import com.example.finalprojectsujin221220.repository.UserRepository;
@@ -45,6 +49,30 @@ public class CommentService {
                 .userName(authentication.getName())
                 .postId(postId)
                 .createdAt(savedComment.getCreatedAt())
+                .build();
+
+    }
+
+    public CommentModifyResponse modifyComment(Long postId, Long id, CommentModifyRequest dto, Authentication authentication) {
+        User user = ur.findByUserName(authentication.getName())
+                .orElseThrow(() -> new ApplicationException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()));
+        Post post = pr.findById(postId)
+                .orElseThrow(() -> new ApplicationException(ErrorCode.POST_NOT_FOUND, ErrorCode.DUPLICATED_USER_NAME.getMessage()));
+        Comment comment = cr.findById(id)
+                .orElseThrow(() -> new ApplicationException(ErrorCode.COMMENT_NOT_FOUND, ErrorCode.COMMENT_NOT_FOUND.getMessage()));
+        if( !post.getUser().equals(user)) throw new ApplicationException(ErrorCode.INVALID_PERMISSION, ErrorCode.INVALID_PERMISSION.getMessage());
+
+        comment.setComment(dto.getComment());
+        comment.setLastModifiedAt(LocalDateTime.now());
+        Comment savedComment = cr.save(comment);
+
+        return CommentModifyResponse.builder()
+                .id(savedComment.getCommentId())
+                .comment(savedComment.getComment())
+                .userName(authentication.getName())
+                .postId(postId)
+                .createdAt(savedComment.getCreatedAt())
+                .lastModifiedAt(savedComment.getLastModifiedAt())
                 .build();
 
     }
