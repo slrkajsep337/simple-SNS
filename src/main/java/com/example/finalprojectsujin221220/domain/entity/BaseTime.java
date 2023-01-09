138a9c3104acec426861b2baff4194765dddf955
이수진 <slrkajsep337@naver.com>
2023-01-09 16:45:31 +0900

    feat : time zone, 출력타입 맞춰주기

--- src/main/java/com/example/finalprojectsujin221220/domain/entity/BaseTime.java
+++ src/main/java/com/example/finalprojectsujin221220/domain/entity/BaseTime.java
@@ -1,9 +1,10 @@
 package com.example.finalprojectsujin221220.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
@@ -20,11 +21,14 @@ public abstract class BaseTime {
 
     @CreatedDate
     @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
 
-    @LastModifiedBy
+    @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime lastModifiedAt;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime deletedAt;
 
 
